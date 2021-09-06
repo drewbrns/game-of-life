9b3d7ad9c9751bcb5e8e223145ce995ac7a7116e
Drew Barnes <djblin@gmail.com>
2021-09-06 08:48:32 +0000

    Modify code

--- run_game.py
+++ run_game.py
@@ -1,9 +1,9 @@
 # /usr/bin/env python
-from game.ui import GameOfThrones
+from game.ui import GameOfLife
 from game.utility.config import Preferences
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     p = Preferences()
-    game_of_Thrones = GameOfThrones(p.rows, p.columns, p.ratio)
+    game_of_Thrones = GameOfLife(p.rows, p.columns, p.ratio)
     game_of_Thrones.run()
